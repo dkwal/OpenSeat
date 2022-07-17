91d1a3aba3d98cccfcdd192db9305e89ff93284b
Daniel Wallace <d.keithwallace@gmail.com>
2022-07-17 14:51:06 -0700

    push before implementing restaurant show page

--- frontend/components/restaurant_index/restaurant_index_item.jsx
+++ frontend/components/restaurant_index/restaurant_index_item.jsx
@@ -1,24 +1,37 @@
 import React from "react"
+import { withRouter } from "react-router-dom";
 
-const RestaurantIndexItem = ({ restaurant }) => {
+class RestaurantIndexItem extends React.Component {
 
-    return(
-        <div className="restaurant-index-item">
-            <div className="restaurant-name">{restaurant.name}</div>
-            <div className="ratings-reviews">
-                <div className="overall-rating">Overall rating</div>
-                <div className="review-count">Num reviews</div>
+    constructor(props) {
+        super(props)
+        this.updatePath = this.updatePath.bind(this);
+    }
+
+    updatePath() {
+        let path = `/api/restaurants/${this.props.restaurant.id}`;
+        this.props.history.push(path);
+    }
+
+    render() {
+        return(
+            <div className="restaurant-index-item" onClick={this.updatePath}>
+                <div className="restaurant-name">{this.props.restaurant.name}</div>
+                <div className="ratings-reviews">
+                    <div className="overall-rating">Overall rating</div>
+                    <div className="review-count">Num reviews</div>
+                </div>
+                <div className="restaurant-details">
+                    <div className="food-type">{this.props.restaurant.food_type} &#11825; </div>
+                    <div className="price-range">{this.props.restaurant.price_range}</div>
+                </div>
+                <div className="timeslot-buttons">
+                    <button>5:30 pm</button>
+                    <button>6:00 pm</button>
+                </div>
             </div>
-            <div className="restaurant-details">
-                <div className="food-type">{restaurant.food_type} &#11825; </div>
-                <div className="price-range">{restaurant.price_range}</div>
-            </div>
-            <div className="timeslot-buttons">
-                <button>5:30 pm</button>
-                <button>6:00 pm</button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default RestaurantIndexItem;
+export default withRouter(RestaurantIndexItem);
